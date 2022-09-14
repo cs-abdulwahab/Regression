019318d1cc59cf6b6c430194204a38fd14880f91
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2022-09-14 21:03:29 +0500

    init

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3,
-                                                    random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3,   random_state=0)
 # Fitting Simple Linear Regression to the Training set
 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+
+#   get MAE   or  RMSE   or   MSE
